957eaabe1dd637b806f6242ea25136f2c23f9d25
Lukasz Holc <lukasz.holc@bluestonepim.com>
2025-01-12 11:07:18 +0100

    Changed to vite instead webpack. Fixed bug where workbook has only one ds.

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true, // Enable source maps for production builds
+  },
+  server: {
+    sourcemap: "inline",
+    port: 3000,
+  },
+});
